5c95a86911ef7cbeeaeca8bb948841f1c506b3f8
Jose Luis Nunez <juniorng10@gmail.com>
2025-07-26 17:28:37 +0000

    Created User and Tasks models for database

--- src/api/models.py
+++ src/api/models.py
@@ -6,37 +6,38 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    tasks = db.relationship('Tasks', backref='user', lazy=True)
+
     def __repr__(self):
         return f'<User {self.email}>'
     
-    tasks = db.relationship('Tasks', backref='user', lazy=True)
+    
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "name": self.name,
-           
+            "username": self.username,
         }
     
 
-    class Tasks(db.model):
-        id = db.Column(db.Integer, primary_key=True)
-        label = db.Column(db.String(120), unique=True, nullable=False)
-        completed = db.Column(db.Boolean(), unique=False, nullable=False)
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    completed = db.Column(db.Boolean(), unique=False, nullable=False)
         
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-        def serialize(self):
-            return {
+    def serialize(self):
+        return {
                 "id":self.id,
                 "label": self.label,
                 "completed":self.completed,
                 "user_id":self.user.id,
-            }
+        }
 
