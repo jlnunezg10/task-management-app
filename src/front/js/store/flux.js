ed6e16d9712278a4a61d21fd3197c14acb64f462
Jose Luis Nunez <juniorng10@gmail.com>
2025-07-27 04:31:36 +0000

    Se ha creado los actions en el flux de register, login, add_task y edit_task

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,4 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
+
+
+	const apiURL = process.env.BACKEND_URL;
 	return {
 		store: {
 			message: null,
@@ -13,7 +16,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			users:[],
+			tasks:[],
+
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -46,7 +52,191 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+
+			//action para registrar un nuevo usuario
+			register: async (username,email,password) => {
+
+				try {
+
+					const urlApi = `${apiURL}/api/register`
+
+					if (!username){
+						console.log("Datos incompletos, por favor especificar username")
+						return false;
+					}
+					if (!email){
+						console.log("Datos incompletos, por favor especificar email")
+						return false;
+					}
+					if (!password){
+						console.log("Datos incompletos, por favor especificar contraseña")
+						return false;
+					}
+
+					const result = await fetch(urlApi, {
+						method:"POST",
+						body:JSON.stringify({
+							username:username,
+							email: email,
+							password: password
+						}),
+						headers: {
+							"Content-type": "application/json; charset=UTF-8"
+						}
+					});
+
+					if (!result.ok){
+						console.error("Error en registro de usuario")
+						throw new Error(result.status)
+					}
+
+					const data = await result.json()
+					console.log("Registro Exitoso")
+					console.log(data)
+					return true;	
+
+				} catch (error) {
+					console.error("Se presenta el siguiente error:", error)
+					return false;
+					
+				}
+			},
+
+			// action para loguear un nuevo usuario
+			login: async(email,password) => {
+
+				const urlApi = `${apiURL}/api/login`
+
+				try {
+
+					if (!email){
+						console.log("Datos incompletos, por favor especificar email")
+						return false
+					}
+					if (!password){
+						console.log("Datos incompletos, por favor especificar contraseña")
+						return false
+					}
+
+					const result = await fetch(urlApi, {
+						method:"POST",
+						body:JSON.stringify({
+							email: email,
+							password: password
+						}),
+						headers: {
+							"Content-type": "application/json; charset=UTF-8"
+						}
+					});
+
+					if (!result.ok){
+						console.error("Error en login del usuario")
+						throw new Error(result.status)
+					}
+
+					const data = await result.json();
+
+					if (!data.access_token) {
+						console.error(data, "No se recibio un token valido")
+						throw new Error("No token received");
+					}
+
+					localStorage.setItem("token", data.access_token)
+					console.log("Logueo Exitoso")
+					return true;
+
+					
+				} catch (error) {
+					console.error("Se presenta el siguiente error:", error)
+					return false;
+					
+				}
+
+				},
+				// action para agregar una nueva tarea
+			add_task: async(label) =>{
+				const urlApi = `${apiURL}/api/tasks`
+				try {
+
+					if(!label){
+						console.error("Falta descripcion de la tarea")
+						return false;
+					}
+
+					const result = await fetch(urlApi, {
+						method:"POST",
+						body:JSON.stringify({
+							label:label
+						}),
+						headers: {
+							"Authorization": `Bearer ${localStorage.getItem('token')}`,
+							"Content-type": "application/json; charset=UTF-8"
+						}
+					});
+
+					if (!result.ok){
+						console.error("Error en agregar task")
+						throw new Error(result.status)
+
+					}
+
+					const data = await result.json()
+					console.log("Tarea agregada exitosamente")
+					setStore({...store,tasks:[...getStore().tasks, data]})
+					return true;	
+
+					
+				} catch (error) {
+					console.error("Se presenta el siguiente error:", error)
+					return false;
+					
+				}
+			},
+
+			edit_task: async(task_id,label) =>{
+				try {
+					const urlApi = `${apiURL}/api/tasks/${task_id}`
+
+					if(!label){
+						console.error("No hay dato que editar")
+						return false;
+					}
+
+
+					const result = await fetch(urlApi, {
+						method:"PUT",
+						body:JSON.stringify({
+							label:label
+						}),
+						headers: {
+							"Authorization": `Bearer ${localStorage.getItem('token')}`,
+							"Content-type": "application/json; charset=UTF-8"
+						}
+					});
+
+					
+					if (!result.ok){
+						console.error("Error en editar task")
+						throw new Error(result.status)
+
+					}
+
+					const data = await result.json()
+					setStore({...store,tasks:[...getStore().tasks, data]})
+					console.log("Tarea editada")
+					return true;
+					
+				} catch (error) {
+					console.error("Se presenta el siguiente error:", error)
+					return false;
+				}
+
+
 			}
+
+
+
 		}
 	};
 };
